2ce00d0624ea2a31a9b8d90ef93b10304e4b0b4b
Siya Jariwala <siya.jari14@gmail.com>
2025-08-20 15:19:47 -0700

    Fixed config

--- MyToDo/frontend/src/config.js
+++ MyToDo/frontend/src/config.js
@@ -1,5 +1,2 @@
-const API_BASE_URL = import.meta.env.PROD 
-  ? 'https://mytodo-backend-v2.onrender.com'
-  : 'http://localhost:5001';
-
+const API_BASE_URL = 'https://mytodo-backend-v2.onrender.com';
 export default API_BASE_URL;
